ed342cb53a5a2e32e558eb4f06dcfb06032202fe
chamster24 <227488190+chamster24@users.noreply.github.com>
2026-04-17 17:33:48 -0600

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 REPO="chamster24/Crostini-Killer"
 # Get the tag name of the latest stable release
 LATEST=$(curl -s https://api.github.com/repos/$REPO/releases/latest | grep "tag_name" | cut -d '"' -f 4)
