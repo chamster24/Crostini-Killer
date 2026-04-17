45ffd0a45a42602f5119db819faed90c1739d204
chamster24 <227488190+chamster24@users.noreply.github.com>
2026-04-17 17:35:31 -0600

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -4,4 +4,4 @@ REPO="chamster24/Crostini-Killer"
 # Get the tag name of the latest stable release
 LATEST=$(curl -s https://api.github.com/repos/$REPO/releases/latest | grep "tag_name" | cut -d '"' -f 4)
 # Run the installer
-curl -sSL "https://raw.githubusercontent.com/$REPO/$LATEST/installer.sh" | bash
+bash <(curl -sSL "https://raw.githubusercontent.com/$REPO/$LATEST/installer.sh")
