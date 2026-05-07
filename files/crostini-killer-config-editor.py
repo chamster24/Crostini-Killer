3fe1bb00fd96e2b0f68c3a3152cb5f759474743e
chamster24 <227488190+chamster24@users.noreply.github.com>
2026-05-07 12:52:35 -0600

    Update crostini-killer-config-editor.py

--- files/crostini-killer-config-editor.py
+++ files/crostini-killer-config-editor.py
@@ -15,3 +15,30 @@ userinput = None
 # ANSI Escape Sequences
 fullscreenwipe = "\033c" # also resets colors
 clearscreen = "\033[2J" # leaves cursor be
+
+#Read the .csv config file
+rawconfig = None
+try:
+	base_path = os.path.dirname(__file__)
+	config_path = os.path.join(base_path, "crostini-killer-config.csv")
+	with open(config_path, 'r', newline="") as file:
+		rawconfig = list(csv.reader(file))
+except Exception:
+	print("Error opening file. Are you sure it's in the same folder as this program?\nQuitting...")
+	time.sleep(5)
+	sys.exit(66)
+
+
+# Main Process
+while True:
+  pass
+
+# Script to write it
+def savechanges():
+  try:
+    with open(config_path, "w", newline="") as file:
+      writer = csv.writer(file)
+      writer.writerows(rawconfig)
+    print("Rewrote config file with the latest changes!")
+  except Exception:
+    print("An error occured while writing to the config file!")
