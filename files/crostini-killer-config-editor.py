af0317ea23c5e60bd36b3d58a17a6c63df81fbe6
chamster24 <227488190+chamster24@users.noreply.github.com>
2026-05-13 09:15:59 -0600

    Update crostini-killer-config-editor.py

--- files/crostini-killer-config-editor.py
+++ files/crostini-killer-config-editor.py
@@ -10,7 +10,9 @@ import sys
 import csv
 import time
 import subprocess
+import curses
 userinput = None
+keylist = []
 
 # ANSI Escape Sequences
 fullscreenwipe = "\033c" # also resets colors
@@ -29,16 +31,90 @@ except Exception:
 	sys.exit(66)
 
 
+# Script to write the changes
+def savechanges(): #add making a temp file, and using that temp file to replace the directory instead
+	try:
+		with open(config_path, "w", newline="") as file:
+			writer = csv.writer(file)
+			writer.writerows(rawconfig)
+			print("Rewrote config file with the latest changes!")
+	except Exception:
+		print("An error occured while writing to the config file!")
+		
+# Define main processes
+def readcsv():
+	pass
+
+def editcsv():
+	global keylist
+	# Add script to move the edit cursor to the latest
+	curses.filter()
+	stdscr = curses.initscr()
+	stdscr.keypad(True)
+
+	try:
+		while True:
+			ch = stdscr.getch()
+			if ch == curses.KEY_UP:
+				if not "UP" in keylist:
+					keylist.append("UP")
+			else:
+				if "UP" in keylist:
+					keylist.remove("UP")
+			if ch == curses.KEY_DOWN:
+				if not "DN" in keylist:
+					keylist.append("DN")
+			else:
+				if "DN" in keylist:
+					keylist.remove("DN")
+			if ch == curses.KEY_LEFT:
+				if not "LT" in keylist:
+					keylist.append("LT")
+			else:
+				if "LT" in keylist:
+					keylist.remove("LT")
+			if ch == curses.KEY_RIGHT:
+				if not "RT" in keylist:
+					keylist.append("RT")
+			else:
+				if "RT" in keylist:
+					keylist.remove("RT")
+            		if ch == curses.ESCAPE:
+				print("Quitting...")
+				time.sleep(1.5)
+                break
+	finally:
+	        # 3. Clean up
+	        curses.endwin()
+
+def help():
+	print("No Help Documentation Yet")
+	time.sleep(3)
+
+def quitprogram():
+	userinput = input("Are you sure you want to quit?\nPress 'Y' and then press enter to quit.\nPress any other key and enter to return to program.")
+	if userinput.upper() == "Y":
+		print("Leaving config editor.")
+		time.sleep(3)
+		quit()
+	else:
+		print("Returning.")
+		time.sleep(2)
+
 # Main Process
 while True:
-  pass
+	print(clearscreen)
+	print("Crostini Killer Config File Editor\n----------")
+	userinput = input("- Menu -\n1. Read CSV config file\n2. Edit CSV config file\n3. Help on this program\n4. Quit")
+	if userinput.isdigit() and int(userinput) == 1:
+		readcsv()
+	elif userinput.isdigit() and int(userinput) == 2:
+		editcsv()	
+	elif userinput.isdigit() and int(userinput) == 3:
+		help()
+	elif userinput.isdigit() and int(userinput) == 4:
+		quitprogram()
+	else:
+		print("Invalid input. Try again.")
+		time.sleep(1.5)
 
-# Script to write it
-def savechanges(): #add making a temp file, and using that temp file to replace the directory instead
-  try:
-    with open(config_path, "w", newline="") as file:
-      writer = csv.writer(file)
-      writer.writerows(rawconfig)
-    print("Rewrote config file with the latest changes!")
-  except Exception:
-    print("An error occured while writing to the config file!")
